236bf721a86902389cca44a23c44401ceaa3c3fe
juandiegogiraldocardona02-del <juandiegogiraldocardona02@gmail.com>
2026-05-08 11:50:24 -0500

    "Ejemplos dart terminados"

---
+++ PrimerTrimestre/programacion/Dart_ADSO/ejemplos_dart/ejemplo4.dart
@@ -0,0 +1,60 @@
+// Condicionales
+
+// Condicional simple
+
+void main(List<String> args) {
+  int edad = 18;
+  if (edad >= 18) {
+    print("Acceso permitido");
+  }
+  // Condicional doble
+
+  double valorNota;
+  valorNota = 3.8;
+  if (valorNota < 3) {
+    print('no aprueba la nota');
+  } else {
+    print('aprueba la nota');
+  }
+
+  // Condicional anidado
+
+  bool tieneCuenta = true;
+  double saldo = 450000;
+  if (tieneCuenta == true) {
+
+    // Se valida si el usuario tiene cuenta
+
+    if (saldo >= 100000) {
+
+      // Se valida si el saldo es mayor o igual a 100000
+
+      print('Puede retirar dinero');
+    } else {
+      print('no puede retirar dinero');
+    }
+  } else {
+    print('Usted no tiene cuenta en este banco');
+  }
+
+  // Condicional Multiple
+  
+  String dia = 'martes';
+  switch (dia){
+    case 'Lunes':
+    print('todos hacen el aseo');
+    break;
+    case 'martes':
+    print('Dejar el repositorio Full en GitHub');
+    break;
+    case 'Miercoles':
+    print('Entrega de evidencias');
+    break;
+    case 'Sabado':
+    case 'Domingo':
+    print('No hay formacion');
+    break;
+    default:
+    print('Dia normal de formacion');
+    }
+}
